2321e704559bf1c81ccd9a74b046ee2a879ac711
Scott Boggs <scott@tams.tech>
2020-12-06 11:06:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# xdg
+
+Constants representing the XDG config locations or their standard defaults if not set.
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     xdg:
+       github: dscottboggs/xdg
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "xdg"
+
+File.open XDG::CONFIG::HOME / "myapp" / "config.yml", mode: "w" do |config|
+  myapp_config.to_yaml config
+end
+```
---
+++ src/xdg.cr
@@ -0,0 +1,34 @@
+# XDG standard values
+
+def envpath?(name) : Path?
+  if set = ENV[name]?
+    return if set.empty?
+    Path.new set
+  end
+end
+
+def env_list_of_paths?(name) : Array(Path)?
+  if (paths = ENV[name]?) && !paths.empty?
+    paths.split ':'
+  end
+end
+
+module XDG
+  module DATA
+    HOME = (envpath? "XDG_DATA_HOME") || Path.home / ".local" / "share"
+    DIRS = (env_list_of_paths? "XDG_DATA_DIRS") || ["/usr/local/share/", "/usr/share/"]
+  end
+
+  module CONFIG
+    HOME = (envpath? "XDG_CONFIG_HOME") || Path.home / ".config"
+    DIRS = (env_list_of_paths? "XDG_CONFIG_DIRS") || ["/etc/xdg"]
+  end
+
+  module CACHE
+    HOME = (envpath? "XDG_CACHE_HOME") || Path.home / ".cache"
+  end
+
+  module RUNTIME
+    DIR = envpath? "XDG_RUNTIME_DIR"
+  end
+end
